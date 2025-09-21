fbcbb638bb1cb7387757a2c00401f8c120e7b1e2
anggarasa <anggasaputra6259@gmail.com>
2025-09-21 16:23:57 +0700

    latihan ke satu di hari ke tujuh, minggu ke satu, bulan ke tiga, tahun ke satu

---
+++ tahun_ke_1/bulan_ke_3/minggu_ke_1/hari_ke_7/remove_spasi.dart
@@ -0,0 +1,14 @@
+String noSpace(String x) {
+  return x.replaceAll(' ', '');
+}
+
+void main() {
+  print(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
+  // Output: 8j8mBliB8gimjB8B8jlB
+
+  print(noSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
+  // Output: 88Bifk8hB8BB8BBBB888chl8BhBfd
+
+  print(noSpace("8aaaaa dddd r     "));
+  // Output: 8aaaaaddddr
+}
