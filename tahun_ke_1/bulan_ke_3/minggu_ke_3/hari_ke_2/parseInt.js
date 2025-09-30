9bfe75c9a55959a45d7480a957fabea9449e7195
Production ZMI <devzenmultimedia@gmail.com>
2025-09-30 08:00:29 +0700

    latihan ke satu di hari ke dua, minggu ke tiga, bulan ke tiga, tahun ke satu

---
+++ tahun_ke_1/bulan_ke_3/minggu_ke_3/hari_ke_2/parseInt.js
@@ -0,0 +1,12 @@
+function getAge(inputString) {
+  return parseInt(inputString[0]);
+}
+
+console.log(getAge("4 years old"));
+// 4
+
+console.log(getAge("9 years old"));
+// 9
+
+console.log(getAge("0 years old"));
+// 0
