a1ae5700091958126787375349b365d19967ff4b
anggarasa <anggasaputra6259@gmail.com>
2025-10-29 08:04:19 +0700

    latihan ke satu di hari ke tiga, minggu ke tiga, bulan ke empat, tahun ke satu

---
+++ tahun_ke_1/bulan_ke_4/minggu_ke_3/hari_ke_3/multification_table.js
@@ -0,0 +1,9 @@
+function multiTable(number) {
+  let result = [];
+  for (let i = 1; i <= 10; i++) {
+    result.push(`${i} * ${number} = ${i * number}`);
+  }
+  return result.join("\n");
+}
+
+console.log(multiTable(5));
